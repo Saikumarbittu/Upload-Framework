a378beebe6c8612aaa4b7bd504e7b7aa4b5ccccd
Saikumar <deshaganisaikumar@gmail.com>
2025-03-26 21:55:23 +0530

    4

--- cypress/e2e/Naukri.cy.js
+++ cypress/e2e/Naukri.cy.js
@@ -2,7 +2,7 @@ import {LoginNaukri, UploadResume} from '../Pages/Naukri.cy'
 
 
 describe('template spec', () => {
-  it('Login Naukri & Upload Resume ', () => {
+  it.only('Login Naukri & Upload Resume ', () => {
     UploadResume();
     cy.task('logMessage',"Uploaded Resume in Naukri succesfully")
   })
